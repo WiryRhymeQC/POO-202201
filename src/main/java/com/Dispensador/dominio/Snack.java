282e7d8c9b2876c705a1d49fda5e6febb22c8457
Jhonatanelpro <jhonatanelpro2014@hotmail.com>
2022-03-28 16:54:18 -0500

    Taller

---
+++ src/main/java/com/Dispensador/dominio/Snack.java
@@ -0,0 +1,17 @@
+package com.Dispensador.dominio;
+
+public class Snack {
+    public String TipoSnack;
+    public long Cantidad;
+    public short codigo;
+    public long precio;
+
+
+
+    public void Retirar(String snack,long cantidad,long valor,short code ){
+
+
+
+    }
+
+}
